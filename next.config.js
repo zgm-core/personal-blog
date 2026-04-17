d3df261ec24f5450d0d1d5aee9a43a709f410442
ZGM <2273204759@qq.com>
2026-04-17 15:13:55 +0800

    fix: add turbopack config for Next.js 16 compatibility

--- next.config.js
+++ next.config.js
@@ -70,9 +70,7 @@ module.exports = () => {
     reactStrictMode: true,
     trailingSlash: false,
     pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
-    eslint: {
-      dirs: ['app', 'components', 'layouts', 'scripts'],
-    },
+    turbopack: {},
     images: {
       remotePatterns: [
         {
